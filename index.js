aefbf76fadc021f2b2a7556ed42736df1494ccfd
Nadine Fischoff <nadine.khattak@gmail.com>
2017-02-06 21:02:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function iterativeLog(array) {
+  array.forEach((item, index, array) => {
+    console.log(`${index}: ${item}`)
+  })
+}
+
+function iterate(callback) {
+  array = [1,2,3]; 
+  array.forEach(callback); 
+  return array; 
+  
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
