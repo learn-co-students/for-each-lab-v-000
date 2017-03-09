17751f30ccc7af1a7ac540c4f7ff3229e3322578
lizdenhup <lizdenhup@gmail.com>
2017-03-09 14:17:18 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  })
+}
+
+function iterate(callback) {
+  var letters = ['a', 'b', 'c'];
+  letters.forEach(callback);
+  return letters;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+  return array;
+}
