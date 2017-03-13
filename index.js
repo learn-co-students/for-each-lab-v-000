ab294a27282e57c7c37c50267a81325a09c04b31
Christian Whitesides <whitesidescr@gmail.com>
2017-03-13 15:09:27 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var pets = ["dog", "cat", "fish"]
+  pets.forEach(callback)
+
+  return pets
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
