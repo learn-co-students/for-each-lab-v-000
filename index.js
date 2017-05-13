73089c303e8bab873931c64212eb0114a85f9194
bentonwong <bentonwong@gmail.com>
2017-05-12 17:07:35 -0700

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  for (var i = 0; i < array.length; i++) {
+    console.log(`${i}: ${array[i]}`)
+  }
+}
+
+function iterate(callback) {
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
