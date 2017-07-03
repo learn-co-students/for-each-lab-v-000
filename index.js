8da2fcbc4cba4e888ad217dba9afce80baccdc30
Zenia <zeniavilla@hotmail.com>
2017-07-03 09:20:02 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+    array.forEach((element, index) => {
+        console.log(`${index}: ${element}`);
+    });
+}
+
+function iterate(callback) {
+    var array = ['maui', 'kyoto', 'osaka'];
+
+    array.forEach(callback);
+
+    return array;
+}
+
+function doToArray(array, callback) {
+    array.forEach(callback);
+}
