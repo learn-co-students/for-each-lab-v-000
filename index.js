5b12defbb90ba851127aaa9d6f16b453120a87cf
jordan surgick <jordansurgick@aol.com>
2017-07-09 20:59:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+   array.forEach((element, index) => {
+     console.log(`${index}: ${element}`)
+   })
+}
+
+function iterate(callback) {
+  var ani = [`dogs`, `cats`, `frogs`]
+
+  ani.forEach(callback)
+
+  return ani
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
