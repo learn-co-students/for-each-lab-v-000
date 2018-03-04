8d1029db0b86a935d76ae5d5a5afc69a793835e1
smithwebtek <brad@smithwebtek.com>
2018-03-04 18:35:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+var dogs = ['shepherd', 'boxer', 'schnauzser', 'pug'];
+
+
 function iterativeLog(array) {
-  array.forEach(logger);
+	array.forEach(logger);
 }
 
-function logger(element, index){
-  console.log(`${index}: ${element}`);
+function logger(element, index) {
+	console.log(`${index}: ${element}`);
 }
 
 function iterate(callback) {
-  var pets = ['fish', 'dog', 'cat', 'bird'];
-  pets.forEach(callback);
-  return pets;
+	var pets = ['fish', 'dog', 'cat', 'bird'];
+	pets.forEach(callback);
+	return pets;
 }
 
 function doToArray(array, callback) {
-  array.forEach(callback);
+	array.forEach(callback);
+}
+
+function rev(arr) {
+	let newArr = []
+	arr.forEach((element) => newArr.push(element.split('').reverse().join('')))
+	return newArr
 }
